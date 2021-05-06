35946c63d4dbe06687e2b1e44b1d16f6b099d2ef
Piyush <piyushguptaa2z123@gmail.com>
2021-05-06 10:09:13 +0530

    checking if file exists before deleting

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -76,7 +76,15 @@ exports.updateBlog = async (req, res) => {
         });
       }
       await fs.unlink(`./${updatingBlog.image}`, (err) => {
-        if (err) throw err;
+        if (err && err.code == "ENOENT") {
+          // file doens't exist
+          console.info("File doesn't exist, won't remove it.");
+        } else if (err) {
+          // other errors, e.g. maybe we don't have enough permission
+          console.error("Error occurred while trying to remove file");
+        } else {
+          console.info(`removed`);
+        }
       });
 
       updatedBlog = {
@@ -116,7 +124,15 @@ exports.deleteBlog = async (req, res) => {
       });
     }
     await fs.unlink(`./${deletingBlog.image}`, (err) => {
-      if (err) throw err;
+      if (err && err.code == "ENOENT") {
+        // file doens't exist
+        console.info("File doesn't exist, won't remove it.");
+      } else if (err) {
+        // other errors, e.g. maybe we don't have enough permission
+        console.error("Error occurred while trying to remove file");
+      } else {
+        console.info(`removed`);
+      }
     });
 
     deletingBlog.delete();
